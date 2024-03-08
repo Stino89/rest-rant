c7343112573a891aa7ecfdc40f08bcd9e05050ed
Stino89 <ftr1989@gmail.com>
2024-03-07 16:49:29 -0800

    adding home page view

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const express = require('express')
-require('dotenv').config()
 
+require('dotenv').config()
+const express = require('express')
 const app = express()
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+  res.send('404 page')
 })
 
-XX
-const PORT = process.env.PORT || 3000
+app.listen(process.env.PORT)
 
-app.listen(PORT, console.log(`listening on port ${PORT}`))
+
+
+
+
+
+
+
+
+
+
+
