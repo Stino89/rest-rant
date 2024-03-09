25ca4d07dbb00bffd29baefdcb078d536b51e136
Stino89 <ftr1989@gmail.com>
2024-03-08 22:09:25 -0800

    view/places

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,5 +1,35 @@
-// GET /places
-app.get('/', (req, res) => {
-    res.render('places/index')
-  })
-  res.render('places/index', { places })
+const React = require('react')
+const Def = require('../default')
+
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div className="col-sm-6">
+          <h2>
+            <a href={`/places/${place.id}`} >
+              {place.name}
+            </a>
+          </h2>
+          <p className="text-center">
+            {place.cuisines}
+          </p>
+          <img src={place.pic} alt={place.name} />
+          <p className="text-center">
+            Located in {place.city}, {place.state}
+          </p>
+        </div>
+      )
+    })
+    return (
+      <Def>
+          <main>
+              <h1>Places to Rant or Rave About</h1>
+              <div className="row">
+                {placesFormatted}
+              </div>
+          </main>
+      </Def>
+    )
+  }
+
+module.exports = index
