67e0354a246e9e324d4605964f9b8cb2ceb65d3c
Stino89 <ftr1989@gmail.com>
2024-03-10 13:08:14 -0700

    1:08

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-
+// modules and globals
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
+//EXPRESS Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
+//Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -16,6 +22,7 @@ app.get('*', (req, res) => {
     res.render('error404')
   })
   
+  //LISTENER
   const PORT = process.env.PORT || 3000
 
   app.listen(PORT, console.log(`listening on port ${PORT}`)) 
